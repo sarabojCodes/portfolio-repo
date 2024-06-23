b1686fce8987ff20c0db500b260f642f2f739ea8
Saraboj Dev <saraboj@Sarabojs-MacBook-Pro.local>
2024-06-23 10:38:05 +0530

    update .json

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,104 +1,119 @@
-"use client"
-import Image from 'next/image'
-import React, { useEffect } from 'react'
-import { motion } from 'framer-motion'
-import Link from 'next/link'
-import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
-import { HiDownload } from 'react-icons/hi'
-import { FaGithubSquare } from 'react-icons/fa'
-import profile from '@/public/profile.png'
+"use client";
+import Image from "next/image";
+import React, { useEffect } from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import profile from "@/public/profile.png";
 
-import { useSectionInView } from '@/lib/hookes'
-import { useAcitveSectionContext } from '@/context/active-section-context'
+import { useSectionInView } from "@/lib/hookes";
+import { useAcitveSectionContext } from "@/context/active-section-context";
 // import resume from '@/public/CV.pdf'
 const Intro = () => {
-    const { ref } = useSectionInView({ sectionName: "Home", threshold: 0.5 })
-    const  {setActiveSection,setTimeOfLastClick} = useAcitveSectionContext()
+  const { ref } = useSectionInView({ sectionName: "Home", threshold: 0.5 });
+  const { setActiveSection, setTimeOfLastClick } = useAcitveSectionContext();
 
-
-
-    return (
-        <section ref={ref} id='home' className='mb-2 max-w-[50rem] text-center  sm:mb-0  scroll-mt-[100rem]'>
-            <div className='flex items-center justify-center'>
-                <div className='relative'>
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            type: "tween",
-                            duration: 0.2
-                        }}
-                    >
-                        <Image src={profile} alt='Saraboj portrait'
-                            width={200}
-                            height={200}
-                            quality={"95"}
-                            priority={true}
-                            className='h-28 w-28 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
-                        />
-                    </motion.div>
-                    <motion.span
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            type: "spring",
-                            stiffness: 125,
-                            delay: 0.1,
-                            duration: 0.7
-                        }}
-                        className='text-4xl absolute bottom-0 right-0'>
-                        👋
-                    </motion.span>
-                </div>
-            </div>
-            <motion.h1
-                className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-            >
-                <span className="font-bold">Hello, I'm Saraboj.</span> I'm a{" "}
-                <span className="font-bold">full-stack developer</span> with{" "}
-                <span className="font-bold">3 years</span> of experience. I enjoy
-                building <span className="italic">sites & apps</span>. My focus is{" "}
-                <span className="underline">React (Next.js)</span>.
-            </motion.h1>
-            <motion.div className='flex flex-col sm:flex-row justify-center items-center gap-2 px-4 text-lg font-medium'
-
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                    delay: 0.1
-                }}
-            >
-                <Link href={"#contact"}
-                    className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full
+  return (
+    <section
+      ref={ref}
+      id="home"
+      className="mb-2 max-w-[50rem] text-center  sm:mb-0  scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src={profile}
+              alt="Saraboj portrait"
+              width={200}
+              height={200}
+              quality={"95"}
+              priority={true}
+              className="h-28 w-28 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+            className="text-4xl absolute bottom-0 right-0"
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I&apos;m Saraboj.</span> I&apos;m a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">3 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
+      <motion.div
+        className="flex flex-col sm:flex-row justify-center items-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href={"#contact"}
+          className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full
                     outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition
                      group
-                    '
-                    onClick={() => {
-                        setActiveSection("Contact")
-                        setTimeOfLastClick(Date.now())
-                    }}
-                >
-                    Contact me here
-                    <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
-                </Link>
+                    "
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
 
-                <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition
-                     group cursor-pointer  borderBlack' href='/CV.pdf' download>
-                    Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
-                </a>
-                <a className="bg-white p-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition
-                     group cursor-pointer  borderBlack">
-                    <BsLinkedin />
-                </a>
-                <a className="bg-white p-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition
-                     group cursor-pointer  borderBlack text-[1.35rem]">
-                    <FaGithubSquare />
-                </a>
-            </motion.div>
-        </section>
-    )
-}
+        <a
+          className="bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition
+                     group cursor-pointer  borderBlack"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          className="bg-white p-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition
+                     group cursor-pointer  borderBlack"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          className="bg-white p-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition
+                     group cursor-pointer  borderBlack text-[1.35rem]"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+};
 
-export default Intro
+export default Intro;
